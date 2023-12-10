f4d7340ad3ff5672c8289a9463bb448caa72d9ad
Tomas Longo <tomas.longo.dev@gmail.com>
2023-12-10 17:33:40 +0100

    BasicUI

---
+++ Source/SpaceWar/Public/Steam_Fix_Class.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Steam_Fix_Class.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACEWAR_API USteam_Fix_Class : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+};
